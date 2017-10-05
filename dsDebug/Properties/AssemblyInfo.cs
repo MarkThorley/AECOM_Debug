f4be19380159171555c9d7b3e3909f697b6d11ac
2adicted <dido.nenov@gmail.com>
2017-10-05 20:22:14 +0100

    Initial

---
+++ dsDebug/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("dsDebug")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("dsDebug")]
+[assembly: AssemblyCopyright("Copyright ©  2017")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("59247231-3945-4ac0-9d98-c7a43c520eea")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ dsDebug/dsDebug.cs
@@ -0,0 +1,101 @@
+﻿using Autodesk.DesignScript.Runtime;
+using Autodesk.Revit.Attributes;
+using Autodesk.Revit.DB;
+using Autodesk.Revit.UI;
+using RevitServices.Persistence;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using Proto = Revit.Elements;
+using DS = Autodesk.DesignScript.Geometry;
+using System.Text;
+using System.Globalization;
+
+
+namespace dsDebug
+{
+    [Transaction(TransactionMode.Manual)]
+    public class dsDebugAecom
+    {
+        internal dsDebugAecom()
+        {
+
+        }
+        /// <summary>
+        /// Sort Views on Sheets by Size
+        /// </summary>
+        /// <param name="views"></param>
+        /// <param name="width"></param>
+        /// <param name="height"></param>
+        /// <returns></returns>
+        public static List<List<View>> SortViewportsBySize([DefaultArgument("{}")] IList<IList> views,
+            [DefaultArgument("{}")] int width,
+            [DefaultArgument("{}")] int height)
+        {
+            int iterations = views.Count;
+            double feet = 304.8;
+            int wi = width;
+            int he = height;
+
+            using (AdnRme.ProgressForm form = new AdnRme.ProgressForm("Re-arranging Views", "Processing {0} out of " + iterations.ToString() + " elements", iterations))
+            {
+                RevitServices.Transactions.TransactionManager.Instance.EnsureInTransaction(DocumentManager.Instance.CurrentUIDocument.Document);
+
+                List<List<View>> outlines = new List<List<View>>();
+
+                foreach (var el in views)
+                {
+                    List<View> view_set = new List<View>();
+
+                    if (form.getAbortFlag())
+                    {
+                        return null;
+                    }
+
+                    foreach (var e in el)
+                    {
+                        View v = e as ViewSection;
+
+                        double w = (v.Outline.Max.U - v.Outline.Min.U) * feet;
+                        double h = (v.Outline.Max.V - v.Outline.Min.V) * feet;
+
+                        Parameter view_comment = v.get_Parameter(BuiltInParameter.VIEW_DESCRIPTION);
+
+                        if ((w < wi / 2) && (h < he / 2))
+                        {
+                            view_comment.Set("A");
+                        }
+                        else if ((w > wi / 2) && (w < wi) && (he < h / 4))
+                        {
+                            view_comment.Set("B");
+                        }
+                        else if ((w > wi / 2) && (w < wi) && (he < h / 2))
+                        {
+                            view_comment.Set("C");
+                        }
+                        else if ((w < wi) && (h < he))
+                        {
+                            view_comment.Set("D");
+                        }
+                        else
+                        {
+                            view_comment.Set("E");
+                        }
+
+                        view_set.Add(v);
+                    }
+
+                    form.Increment();
+
+                    outlines.Add(view_set);
+
+                    RevitServices.Transactions.TransactionManager.Instance.TransactionTaskDone();
+                }
+
+                return outlines;
+            }
+        }
+    }
+}
+
